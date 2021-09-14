1b55915f49e27a643aab7415d9403bfac6a7d72a
faridobasura <yoavpozos@comunidad.unam.mx>
2021-09-13 22:12:33 -0500

    Empezando los problemas del Uva

---
+++ UVa/SuperEasy/Text_Quotes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	bool band;
+	char str;
+	band = true;
+	while(scanf("%c", &str) == 1){
+		if(str == '"' && band == true){
+			printf("``");
+			band = !band;
+		}
+		else if(str == '"' && band == false){
+			printf("''");
+			band = !band;
+		}
+		else{
+			printf("%c", str);
+		}
+	}
+	return 0;
+}
